fe25a06676cea0025ffa4eeb43b8db9b300e160e
Zhenyu Ye <673598755@qq.com>
2021-04-08 00:07:28 -0700

    update sns and lambda alias

--- ec2/sns.tf
+++ ec2/sns.tf
@@ -5,6 +5,6 @@ resource "aws_sns_topic" "webapp_sns_topic" {
 resource "aws_sns_topic_subscription" "lambda_sns_topic_subscribe" {
   topic_arn = aws_sns_topic.webapp_sns_topic.arn
   protocol  = "lambda"
-  endpoint  = aws_lambda_function.webapp_lambda.arn
+  endpoint  = aws_lambda_alias.lambda_alias.arn
 }
 
